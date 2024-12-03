d209cee08d2e07769cc15dbb8cb43cb61ba51151
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2024-12-03 17:47:36 +0100

    [add] for 10 email

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const mail = [];
+const ul = document.getElementById('lista-email')
+
+//generare 10 email
+for (let i = 0; i < 10; i++) {
+  axios.get(endpoint)
+    .then(response => { //
+      console.log(response.data.response);
+    })
+}
+

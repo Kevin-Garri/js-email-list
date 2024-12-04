84c78dd5e9d4b7a9ac5594ef1b527d0aa04228a1
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2024-12-04 17:43:22 +0100

    [add] stampo in pagina

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const ul = document.getElementById('lista-email')
 //generare 10 email
 for (let i = 0; i < 10; i++) {
   axios.get(endpoint)
-    .then(response => { //
-      console.log(response.data.response);
+    .then(response => {
+      const email = response.data.response; // Ottengo l'email
+      mail.push(email); // Aggiungo email all'array
+      ul.innerHTML += `<li>${email}</li>`; // Aggiungo direttamente l'HTML
     })
 }
 
